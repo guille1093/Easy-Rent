023f0f918996ce4064492dbb3f73f492fdc91b01
Guillermo Quintana <guille121093@gmail.com>
2022-09-22 20:39:38 -0300

    comments added

--- src/main/java/com/unam/poo/controllers/PanelUsrController.java
+++ src/main/java/com/unam/poo/controllers/PanelUsrController.java
@@ -13,14 +13,24 @@ import java.util.List;
 
 
 @Controller
+//esta es como una ruta general de la clase
 @RequestMapping("/panel")
 public class PanelUsrController {
     @Autowired UsuarioService usuarioService;
+
+    //esta es la ruta especifica de la funcion
+    // entonces queda localhost:8080/panel/panelUsr
     @GetMapping("/panelusr")
     public String panelUsr(Model model) {
+        //esto es lo mismo que POO1
         List<Usuario> usuarios = usuarioService.findAll();
 //        model.addAllAttributes(usuarios);
+        //aca agregamos los atributos al modelo que es lo
+        //que usamos en el html para mostrar los datos
         model.addAttribute("usuarios", usuarios);
+        //retornamos el nombre del JSP que queremos mostrar
+        //cuando se llame a esta funcion por la ruta
+        //en este caso la tabla de usuarios que esta en WEB-INF/jsp/table.jsp
         return "table";
     }
 }
